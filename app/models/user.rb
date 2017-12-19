6355af67b1d051819ff08abece1caaefa77fcced
fandytcc <fandytsui@gmail.com>
2017-12-19 13:33:50 +0100

    generate Profile model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # our stuff
+  has_one :profile
+
+  def full_name
+    return profile.full_name if profile?
+
+    email
+  end
+
+  def profile?
+    profile.present? && profile.persisted?
+  end
 end
