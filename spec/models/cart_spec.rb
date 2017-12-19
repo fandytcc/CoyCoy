76a48475df9ef46e42c35e7a3865a6248318f223
fandytcc <fandytsui@gmail.com>
2017-12-19 16:40:48 +0100

    added tests and updated carts factory

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   it { is_expected.to belong_to(:user) }
+  describe 'association with list_items' do
+    it { is_expected.to have_many(:list_items) }
+    it { is_expected.to have_many(:carted_products) }
+  end
 end
