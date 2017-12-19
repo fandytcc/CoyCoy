30c36b1805262e1f0b84d43d0cb4d538833db709
fandytcc <fandytsui@gmail.com>
2017-12-19 16:55:23 +0100

    added validation for length of strings and texts and prices

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
 
   def full_name
     "#{first_name} #{last_name}"
