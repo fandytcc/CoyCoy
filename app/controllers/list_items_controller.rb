e9da58fa97c8cc87f43403d2d38a2d418f042b47
svanhesteren <s.vanhesteren@gmail.com>
2017-12-21 18:27:08 +0100

    Add to cart button

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,22 +1,28 @@
 class ListItemsController < ApplicationController
+  before_action :set_product, only: [:create]
   before_action :authenticate_user!
 
   def new
-    @list_item = ListItem.new(list_item_params)
+    @list_item = ListItem.new
   end
 
   def create
-    @list_item = current_user.cart.build_list_item(list_item_params)
-    
+
+    @list_item = current_user.cart.list_items.build(cart: current_user.cart, product: @product)
+
     if @list_item.save
-      redirect_to root_path, notice: "added to cart"
+      redirect_to root_path, notice: "#{@product.name} added to your cart."
     end
   end
 
   private
 
+  def set_product
+    @product = Product.find(list_item_params[:product])
+  end
+
   def list_item_params
-    params.require(:list_item).permit(:quantity, :cart, :product)
+    params.permit(:quantity, :cart, :product)
   end
 
 end
